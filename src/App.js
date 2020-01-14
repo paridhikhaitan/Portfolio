e229540429896c5ba009ceac46ce29227a4b4c59
Paridhi Khaitan <paridhi4699@gmail.com>
2020-01-13 23:09:08 -0800

    Final

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './css/App.scss';
-import Home from './pages/Home';
 import {spacer1, spacer2, spacer3, spacer4} from './components/constants';
 import { BrowserRouter } from 'react-router-dom'
 import Main from './components/Main';
 
 import {setConfiguration} from 'react-grid-system';
-import About from './pages/About';
+
 import Navbar from './components/navbar';
-import FBUCodepath from './pages/FBUCodepath';
-import InfoPost from './pages/InfoPost';
-import Misc from './pages/Misc';
 import ContactFooter from './components/footer';
 
 setConfiguration({
